4a6fdd75a7f08332a7b029149842b4b1192fb122
ch-adeel <chadeelhussain31@gmail.com>
2024-07-28 22:31:19 +0500

    initial-commit

--- src/Componenets/Contact.jsx
+++ src/Componenets/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div name="contact" className="w-full h-full bg-gradient-to-b from-black to-gray-800 text-white">
       <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="mt-64">
+        <div className="mt-80">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
             <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
--- src/Componenets/Experience.jsx
+++ src/Componenets/Experience.jsx
@@ -104,7 +104,7 @@ const Experience = () => {
   return (
     <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full md:h-screen">
      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-white">
-        <div className='pb-8 mt-80'>
+        <div className=' mt-80'>
             <p className="text-4xl font-bold border-b-4 border-gray-500 inline ">Experience</p>
             <p className="py-6">These are the technologies I've worked with</p>
         </div>
