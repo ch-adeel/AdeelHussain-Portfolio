8fa2094fde6cd28ee044c4c0f732915e64bf2d18
ch-adeel <chadeelhussain31@gmail.com>
2025-12-21 15:21:05 +0500

    Updated About section and added Business website in portfolio component 😉

--- src/Componenets/About.jsx
+++ src/Componenets/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white py-16"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-4 md:pb-8">
@@ -15,28 +15,32 @@ const About = () => {
 
         <p className="text-base sm:text-lg md:text-xl mt-6">
           I’m a{" "}
-          <a href="https://www.linkedin.com/in/adeel-hussain-139113186/" className="text-yellow-400 hover:underline font-semibold" >
-          {/* <span className="text-yellow-400 font-semibold"> */}
+          <a
+            href="https://www.linkedin.com/in/adeel-hussain-139113186/"
+            className="text-yellow-400 hover:underline font-semibold"
+          >
+            {/* <span className="text-yellow-400 font-semibold"> */}
             Full-Stack MERN Developer
           </a>{" "}
-          and Manual QA Engineer, blending development and testing skills to
-          deliver robust, fault-free digital solutions.
+          Manual QA Engineer, combining development and testing expertise to
+          deliver stable, high-quality web applications.
         </p>
 
         <p className="text-base sm:text-lg md:text-xl mt-4">
           I’m skilled in{" "}
           <span className="underline decoration-pink-500">manual testing</span>,
-          reviewing UI/UX functionality, validating user flows, writing detailed
-          test scenarios, regression testing, writing test cases, bug tracking,
-          API testing with Postman, sprint planning, and Jira ticket creation
-          and management. This combination allows me to deliver{" "}
-          <span className="underline decoration-indigo-500">
-            clean, tested, and reliable features
-          </span>
-          . I’m passionate about building intuitive interfaces, maintaining code
-          quality, and continuously improving user experience. I enjoy
-          collaborating with teams, understanding requirements, and transforming
-          ideas into production-ready solutions.
+          UI/UX validation,user flow analysis, regression testing, writing
+          detailed test scenarios and test cases, API testing using Postman, bug
+          tracking, sprint participation, and Jira ticket lifecycle management.
+          My dual skillset enables me to build and validate features end-to-end,
+          ensuring both functionality and reliability. I am passionate about
+          writing clean code, maintaining quality standards, and delivering
+          seamless, intuitive <a className="underline decoration-lime-200">User experiences</a>. I focus strongly on customer
+          satisfaction, efficient performance, and ensuring the final product
+          works as expected across all flows before release. I enjoy
+          collaborating with cross-functional teams, understanding business
+          requirements, and transforming ideas into production-ready solutions
+          that add measurable value.
         </p>
       </div>
     </div>
--- src/Componenets/Portfolio.jsx
+++ src/Componenets/Portfolio.jsx
@@ -4,6 +4,7 @@ import company1 from "../assets/company.png";
 import company2 from "../assets/xavor.jpg";
 import company3 from "../assets/digitify.png";
 import company4 from "../assets/techveria.jpg";
+import BL1 from "../assets/BL.png";
 
 import { FaPlus, FaMinus } from "react-icons/fa";
 import installNode from "../assets/portfolio/installNode.png";
@@ -68,89 +69,130 @@ const Portfolio = () => {
   };
 
   return (
-    <div
-      name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full min-h-screen text-white py-16"
-    >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-4 md:pb-8">
-          <p className="text-4xl md:text-5xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
-          </p>
-          <p className="py-4">Check out some of my work right here</p>
-        </div>
+    <>
+      <div
+        name="portfolio"
+        className="bg-gradient-to-b from-black to-gray-800 w-full min-h-screen text-white py-16"
+      >
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+          <div className="pb-4 md:pb-8">
+            <p className="text-4xl md:text-5xl font-bold inline border-b-4 border-gray-500">
+              Portfolio
+            </p>
+            <p className="py-4">Check out some of my work right here</p>
+          </div>
+          <div className="max-w-screen-lg mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-6 px-2">
+            {/* Text Section */}
+            <div className="flex flex-col justify-center md:order-1 order-2">
+              <h2 className="text-4xl sm:text-5xl md:text-5xl font-bold text-white mt-8 md:mt-0 mb-4 sm:mb-6">
+                Business Portfolio{" "}
+                <a
+                  href="https://itbglobal.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="text-blue-400 hover:underline"
+                >
+                  ITB
+                </a>
+              </h2>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 sm:px-0">
-          {portfolios.map(({ id, src, title }) => (
-            <div
-              key={id}
-              className="shadow-md shadow-gray-600 rounded-lg overflow-hidden"
-            >
-              <img
-                src={src}
-                alt={title}
-                className="w-full h-auto duration-200 hover:scale-105"
-              />
-              <p className="mt-2 text-center mb-3 font-semibold hover:border-b-2 hover:border-red-200 hover:text-cyan-200 hover:cursor-pointer">
-                {title}
+              <p className="text-gray-400 text-base sm:text-lg md:text-lg mb-6 max-w-md">
+                This application is built using{" "}
+                <a className="underline decoration-zinc-600 hover:decoration-teal-100">
+                  React.js
+                </a> and <a className="underline decoration-neutral-600 hover:decoration-cyan-300">
+                  Tailwind CSS
+                </a>, 
+                and is deployed on <a className="hover:text-green-300">Netlify</a>. It is a responsive business
+                portfolio website that supports both <a className="hover:font-bold">Dark</a> and <a className="hover:font-bold hover:text-white">Light</a> modes for an
+                enhanced user experience.
               </p>
             </div>
-          ))}
-        </div>
 
-        <div name="professional-experience">
-          <div className="max-w-screen-lg mx-auto px-4">
-            <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold  flex justify-center text-center mb-12 border-b-4 border-gray-500 inline-block pt-9">
-              Professional Experience
-            </h2>
+            {/* Image Section */}
+            <div className="flex justify-center md:justify-end order-2 mb-5">
+              <img
+                src={BL1}
+                alt="Business Portfolio"
+                className="rounded-2xl w-2/3 sm:w-2/3 md:w-3/5 lg:w-2/3"
+              />
+            </div>
+          </div>
 
-            <div className="flex flex-col gap-6">
-              {experiences.map((exp) => (
-                <div
-                  key={exp.id}
-                  className="bg-gray-800 rounded-lg overflow-hidden shadow-md"
-                >
-                  {/* Header with Company + Location */}
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 sm:px-0">
+            {portfolios.map(({ id, src, title }) => (
+              <div
+                key={id}
+                className="shadow-md shadow-gray-600 rounded-lg overflow-hidden"
+              >
+                <img
+                  src={src}
+                  alt={title}
+                  className="w-full h-auto duration-200 hover:scale-105"
+                />
+                <p className="mt-2 text-center mb-3 font-semibold hover:border-b-2 hover:border-red-200 hover:text-cyan-200 hover:cursor-pointer">
+                  {title}
+                </p>
+              </div>
+            ))}
+          </div>
+
+          <div name="professional-experience">
+            <div className="max-w-screen-lg mx-auto px-4">
+              <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold  flex justify-center text-center mb-12 border-b-4 border-gray-500 inline-block pt-9">
+                Professional Experience
+              </h2>
+
+              <div className="flex flex-col gap-6">
+                {experiences.map((exp) => (
                   <div
-                    className="flex justify-between items-center cursor-pointer px-6 py-4"
-                    onClick={() => toggleOpen(exp.id)}
+                    key={exp.id}
+                    className="bg-gray-800 rounded-lg overflow-hidden shadow-md"
                   >
-                    <div>
-                      <h3 className="text-xl font-semibold">{exp.company}</h3>                      
-                      <p className="text-gray-400 text-sm">{exp.location}</p>
-                    </div>
-                    <div className="flex items-center space-x-4">
-                        <span className="text-gray-400 text-sm">{exp.duration}</span>
-                      {openId === exp.id ? (
-                        <FaMinus className="text-gray-300" />
-                      ) : (
-                        <FaPlus className="text-gray-300" />
-                      )}
-                    </div>
-                  </div>
-
-                  {/* Content */}
-                  {openId === exp.id && (
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-6 py-4">
+                    {/* Header with Company + Location */}
+                    <div
+                      className="flex justify-between items-center cursor-pointer px-6 py-4"
+                      onClick={() => toggleOpen(exp.id)}
+                    >
                       <div>
-                        <p className="text-gray-300">{exp.description}</p>
+                        <h3 className="text-xl font-semibold">{exp.company}</h3>
+                        <p className="text-gray-400 text-sm">{exp.location}</p>
                       </div>
-                      <div className="flex justify-center md:justify-end">
-                        <img
-                          src={exp.image}
-                          alt={exp.company}
-                          className="rounded-lg w-2/3 md:w-full"
-                        />
+                      <div className="flex items-center space-x-4">
+                        <span className="text-gray-400 text-sm">
+                          {exp.duration}
+                        </span>
+                        {openId === exp.id ? (
+                          <FaMinus className="text-gray-300" />
+                        ) : (
+                          <FaPlus className="text-gray-300" />
+                        )}
                       </div>
                     </div>
-                  )}
-                </div>
-              ))}
+
+                    {/* Content */}
+                    {openId === exp.id && (
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-6 py-4">
+                        <div>
+                          <p className="text-gray-300">{exp.description}</p>
+                        </div>
+                        <div className="flex justify-center md:justify-end">
+                          <img
+                            src={exp.image}
+                            alt={exp.company}
+                            className="rounded-lg w-2/3 md:w-full"
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
