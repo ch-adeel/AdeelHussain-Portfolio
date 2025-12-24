1cb3ef1bd4e3b620fb428dc34de9ef6bbfb8ccf9
ch-adeel <chadeelhussain31@gmail.com>
2025-12-25 02:53:51 +0500

    Updated Meta Tags. About and Home components updated

--- src/Componenets/About.jsx
+++ src/Componenets/About.jsx
@@ -22,9 +22,9 @@ const About = () => {
             className="text-yellow-400 hover:underline font-semibold"
           >
             {/* <span className="text-yellow-400 font-semibold"> */}
-            Full-Stack MERN Developer
+            Full-Stack Developer
           </a>{" "}
-          Manual QA Engineer, combining development and testing expertise to
+          and Manual QA Engineer, combining development and testing expertise to
           deliver stable, high-quality web applications.
         </p>
 
@@ -38,13 +38,61 @@ const About = () => {
           ensuring both functionality and reliability. I am passionate about
           writing clean code, maintaining quality standards, and delivering
           seamless, intuitive{" "}
-          <span className="underline decoration-lime-200">User experiences</span>. I
-          focus strongly on customer satisfaction, efficient performance, and
-          ensuring the final product works as expected across all flows before
-          release. I enjoy collaborating with cross-functional teams,
+          <span className="underline decoration-lime-200">
+            User experiences
+          </span>
+          . I focus strongly on customer satisfaction, efficient performance,
+          and ensuring the final product works as expected across all flows
+          before release. I enjoy collaborating with cross-functional teams,
           understanding business requirements, and transforming ideas into
           production-ready solutions that add measurable value.
         </p>
+        <div className="max-w-6xl mx-auto mt-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="order-2 md:order-1  bg-gradient-to-b from-gray-800 to-black p-6 rounded-xl shadow-sm">
+              <h4 className="text-xl font-semibold mb-4 text-gray-900 dark:text-white">
+                🤖 Engineering Values:
+              </h4>
+              <ul className="text-gray-700 dark:text-gray-300 leading-relaxed space-y-4">
+                <li>
+                🔹 Clean, maintainable, and well-documented code <br /> 
+                </li>
+                <li>
+                🔹 Focus on scalability and performance <br />
+                </li>
+                <li>
+                🔹 Collaborative mindset and strong communication <br />
+                </li>
+                <li>
+                🔹 Continuous learning and improvement
+                </li>
+              </ul>
+            </div>
+            <div className="order-1 md:order-2  bg-gradient-to-b from-gray-800 to-black p-6 rounded-xl shadow-sm">
+              <h4 className="text-xl font-semibold mb-4 text-gray-900 dark:text-white">
+                🧰 Technical Expertise
+              </h4>
+
+              <ul className="space-y-2 text-gray-700 dark:text-gray-300">
+                <li>
+                  <strong>Frontend:</strong> React.js, Redux, Angular, HTML5,
+                  CSS3, SCSS, Tailwind, MUI, BootStrap
+                </li>
+                <li>
+                  <strong>Backend:</strong> Node.js, Express.js, REST APIs,
+                  GraphQL
+                </li>
+                <li>
+                  <strong>Languages:</strong> JavaScript (ES6+), TypeScript
+                </li>
+                <li>
+                  <strong>Tools & DevOps:</strong> Git, Jira,
+                  Agile/Scrum
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -5,64 +5,64 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-<div
-  name="home"
-  className="w-full bg-gradient-to-b from-black via-black to-gray-800 py-16"
->
-  <div className="max-w-screen-lg mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-8 px-4">
-    {/* Text Section */}
-    <div className="flex flex-col justify-center order-1 md:order-1">
-      <h2 className="text-4xl sm:text-5xl md:text-5xl font-bold text-white mt-8 md:mt-0 mb-4 sm:mb-6">
-        I’m a Full-Stack MERN Developer & QA Tester
-      </h2>
-      <p className="text-gray-400 text-base sm:text-lg md:text-lg mb-6 max-w-md">
-        I build modern, responsive web applications using React, Next.js, TypeScript, Tailwind CSS, Bootstrap, Material UI, and backend tools like Express.js & MongoDB. I also ensure apps are reliable through manual testing.
-      </p>
+    <div
+      name="home"
+      className="w-full bg-gradient-to-b from-black via-black to-gray-800 py-16"
+    >
+      <div className="max-w-screen-lg mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-8 px-4">
+        {/* Text Section */}
+        <div className="flex flex-col justify-center order-1 md:order-1">
+          <h2 className="text-4xl sm:text-4xl md:text-4xl font-bold text-white mt-8 md:mt-0 mb-4 sm:mb-6">
+            Software Engineer | <br /> Full-Stack JavaScript Developer
+          </h2>
+          <p className="text-gray-400 text-base sm:text-lg md:text-lg mb-6 max-w-md">
+            I build modern, responsive web applications using React, Next.js,
+            TypeScript, Tailwind CSS, Bootstrap, Material UI, and backend tools
+            like Express.js & MongoDB. I also ensure apps are reliable through
+            manual testing.
+          </p>
 
-      {/* Portfolio Button - visible on desktop under text */}
-      <div className="hidden md:flex md:justify-start">
-        <Link
-          to="portfolio"
-          smooth
-          duration={500}
-          className="group text-white w-fit px-6 py-3 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-        >
-          Portfolio
-          <span className="group-hover:rotate-90 duration-300">
-            <MdKeyboardArrowRight size={25} className="ml-1" />
-          </span>
-        </Link>
+          {/* Portfolio Button - visible on desktop under text */}
+          <div className="hidden md:flex md:justify-start">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        {/* Image Section */}
+        <div className="flex justify-center md:justify-end order-2">
+          <img
+            src={HeroImage}
+            alt="my-picture"
+            className="rounded-2xl w-2/3 sm:w-2/3 md:w-3/5 lg:w-2/3"
+          />
+        </div>
+
+        {/* Portfolio Button - visible on mobile under image */}
+        <div className="flex justify-center md:hidden mt-6 order-3">
+          <Link
+            to="portfolio"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+          >
+            Portfolio
+            <span className="group-hover:rotate-90 duration-300">
+              <MdKeyboardArrowRight size={25} className="ml-1" />
+            </span>
+          </Link>
+        </div>
       </div>
     </div>
-
-    {/* Image Section */}
-    <div className="flex justify-center md:justify-end order-2">
-      <img
-        src={HeroImage}
-        alt="my-picture"
-        className="rounded-2xl w-2/3 sm:w-2/3 md:w-3/5 lg:w-2/3"
-      />
-    </div>
-
-    {/* Portfolio Button - visible on mobile under image */}
-    <div className="flex justify-center md:hidden mt-6 order-3">
-      <Link
-        to="portfolio"
-        smooth
-        duration={500}
-        className="group text-white w-fit px-6 py-3 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-      >
-        Portfolio
-        <span className="group-hover:rotate-90 duration-300">
-          <MdKeyboardArrowRight size={25} className="ml-1" />
-        </span>
-      </Link>
-    </div>
-  </div>
-</div>
-
-
-
   );
 };
 
